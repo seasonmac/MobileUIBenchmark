5f3b728b90947452c06b4d085f9ce37d5cdfe0f6
season <hwopenharmony@gmail.com>
2022-05-01 23:03:40 +0800

    add webapp

---
+++ usecase_1/H5/webpack.config.js
@@ -0,0 +1,100 @@
+const path = require('path');
+
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const appDirectory = path.resolve(__dirname);
+const {presets} = require(`${appDirectory}/babel.config.js`);
+
+const compileNodeModules = [
+  // Add every react-native package that needs compiling
+  // 'react-native-gesture-handler',
+].map((moduleName) => path.resolve(appDirectory, `node_modules/${moduleName}`));
+
+const babelLoaderConfiguration = {
+  test: /\.js$|tsx?$/,
+  // Add every directory that needs to be compiled by Babel during the build.
+  include: [
+    path.resolve(__dirname, 'index.web.js'), // Entry to your application
+    path.resolve(__dirname, 'App.js'), // Change this to your main App file
+    path.resolve(__dirname, 'src'),
+    ...compileNodeModules,
+  ],
+  use: {
+    loader: 'babel-loader',
+    options: {
+      cacheDirectory: true,
+      presets,
+      plugins: ['react-native-web'],
+    },
+  },
+};
+
+const svgLoaderConfiguration = {
+  test: /\.svg$/,
+  use: [
+    {
+      loader: '@svgr/webpack',
+    },
+  ],
+};
+
+const imgLoaderConfiguration = {
+  test: /\.(png|jpe?g|gif)$/,
+  use: [
+    {
+      loader: 'url-loader',
+      options: {
+        limit:8192
+      }
+
+    },
+  ],
+};
+
+const imageLoaderConfiguration = {
+  test: /\.(gif|jpe?g|png)$/,
+  use: {
+    loader: 'url-loader',
+    options: {
+      name: '[name].[ext]',
+    },
+  },
+};
+
+module.exports = {
+  entry: {
+    app: path.join(__dirname, 'index.web.js'),
+  },
+  output: {
+    path: path.resolve(appDirectory, 'dist'),
+    publicPath: '/',
+    filename: '[name].js',
+  },
+  resolve: {
+    extensions: ['.web.tsx', '.web.ts', '.tsx', '.ts', '.web.js', '.js'],
+    alias: {
+      'react-native$': 'react-native-web',
+      /* set alias from react-native-fast-image to react-native-web-fastimage */
+      'react-native-fast-image': '@preflower/react-native-web-fast-image'
+    },
+  },
+  module: {
+    rules: [
+      babelLoaderConfiguration,
+      imageLoaderConfiguration,
+      svgLoaderConfiguration,
+      imgLoaderConfiguration,
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'index.html'),
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      // See: https://github.com/necolas/react-native-web/issues/349
+      __DEV__: JSON.stringify(true),
+    }),
+  ],
+};
